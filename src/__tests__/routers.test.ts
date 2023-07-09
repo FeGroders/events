ddb763b1f2e2bb38205923cf4f9e3a7ea475f901
GrodrZ <fernandogroder@gmail.com>
2023-07-09 11:30:59 -0300

    fix: tests

--- src/__tests__/routers.test.ts
+++ src/__tests__/routers.test.ts
@@ -13,7 +13,7 @@ test('Testando chamada de API GET Eventos', async () => {
   expect(response.data).toBeDefined();
 });
 
-test('Testando chamada de API GET EVento', async () => {
+test('Testando chamada de API GET Evento', async () => {
   const response = await axios.get('http://localhost:4000/api/events/1');
 
   expect(response.status).toBe(200);
@@ -45,11 +45,31 @@ test('Testando chamada de API Login', async () => {
   expect(response.data).toBeDefined();
 });
 
-test('Testando chamada de API Validacao Certificados', async () => {
-  // Não vai validar  
+test('Testando chamada de API Login incorreto', async () => {
   try {
-    const response = await axios.post('http://localhost:4000/api/certificates/validate', {
-      MD5: "123456789"
+    const response = await axios.post('http://localhost:4000/api/login', {
+      Email: "teste@teste.com",
+      Password: "teste1"
+    });
+  } catch (error: any) {
+    expect(error.response.status).toBe(400);
+    expect(error.response.data).toBeDefined();
+  }
+});
+
+test('Testando chamada de API Validacao Certificados', async () => {
+  const response = await axios.post('http://localhost:4000/api/certificates/validate', {
+    md5: "ea66c06c1e1c05fa9f1aa39d98dc5bc1"
+  });
+
+  expect(response.status).toBe(200);
+  expect(response.data).toBeDefined();
+});
+
+test('Testando chamada de API Validacao Certificados - Não validar', async () => {
+  try {
+    await axios.post('http://localhost:4000/api/certificates/validate', {
+      md5: "123456789"
     });
   } catch (error: any) {
     expect(error.response.status).toBe(404);
